dd55f9158bfcdff0b013aaeccc969273ba4e3b7f
ColinChen1 <colinchen1526@gmail.com>
2015-08-16 23:29:28 -0400

    added error page if email has already been entered

---
+++ main/alreadysignedup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <title> blink eyewear</title>
+  <head>
+   <link href="bootstrap.min.css" rel="stylesheet">
+   <link href="font-awesome.min.css" rel="stylesheet">
+   <link href="bootstrap-social.css" rel="stylesheet">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="referral_page.css">
+    
+  </head>
+
+  <body>
+    <div class = "containe-fluid">
+      <div id = "header">
+        <h id = "company"> blink! Eyewear</h>
+        <!-- <h Id = "thanks"> Thank you for signing up </h> -->
+      </div>
+
+      <div class = "row">
+        <div class = "col-xs-12 middle" id = "with_background">
+          <div id = "input_box">
+            <div id = "invite_friends">
+            <p>&nbsp;</p>
+            <h> This Email Has Already Been Entered! </h> 
+            </div>
+          </div>  
+        </div>
+      </div>
+
+      </div>
+
+
+      </div>
+
+    </div>
+  </body>
+
+</html>
