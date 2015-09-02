8ab0eb579321650e6daebfcdca574ade69908a08
matthwang <swfanmatt@gmail.com>
2015-09-01 21:44:45 -0400

    about page

---
+++ main/aboutpage.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+     $('#member1').click(function(){
+    	$('#member1-info').animate({ "bottom": "-=200px" }, "fast" );
+    	$('#member1-info').animate({ "bottom": "+=10px" }, "fast" );
+    	$('#member1-info').animate({ "bottom": "-=10px" }, "fast" );
+    });
+
+    $('#member1-info').click(function(){
+    	$('#member1-info').animate({ "bottom": "-=10px" }, "fast" );
+    	$('#member1-info').animate({ "bottom": "+=210px" }, "fast" );
+    });
+
+    $('#member2').click(function(){
+    	$('#member2-info').animate({ "bottom": "-=200px" }, "fast" );
+    	$('#member2-info').animate({ "bottom": "+=10px" }, "fast" );
+    	$('#member2-info').animate({ "bottom": "-=10px" }, "fast" );
+    });
+
+    $('#member2-info').click(function(){
+    	$('#member2-info').animate({ "bottom": "-=10px" }, "fast" );
+    	$('#member2-info').animate({ "bottom": "+=210px" }, "fast" );
+    });
+
+     $('#member3').click(function(){
+    	$('#member3-info').animate({ "bottom": "-=200px" }, "fast" );
+    	$('#member3-info').animate({ "bottom": "+=10px" }, "fast" );
+    	$('#member3-info').animate({ "bottom": "-=10px" }, "fast" );
+    });
+
+    $('#member3-info').click(function(){
+    	$('#member3-info').animate({ "bottom": "-=10px" }, "fast" );
+    	$('#member3-info').animate({ "bottom": "+=210px" }, "fast" );
+    });
+
+     $('#member4').click(function(){
+    	$('#member4-info').animate({ "bottom": "-=200px" }, "fast" );
+    	$('#member4-info').animate({ "bottom": "+=10px" }, "fast" );
+    	$('#member4-info').animate({ "bottom": "-=10px" }, "fast" );
+    });
+
+    $('#member4-info').click(function(){
+    	$('#member4-info').animate({ "bottom": "-=10px" }, "fast" );
+    	$('#member4-info').animate({ "bottom": "+=210px" }, "fast" );
+    });
+
+     $('#member5').click(function(){
+    	$('#member5-info').animate({ "bottom": "-=200px" }, "fast" );
+    	$('#member5-info').animate({ "bottom": "+=10px" }, "fast" );
+    	$('#member5-info').animate({ "bottom": "-=10px" }, "fast" );
+    });
+
+    $('#member5-info').click(function(){
+    	$('#member5-info').animate({ "bottom": "-=10px" }, "fast" );
+    	$('#member5-info').animate({ "bottom": "+=210px" }, "fast" );
+    });
+
+});
--- main/referral_page.php
+++ main/referral_page.php
@@ -14,6 +14,16 @@
   </head>
 
   <body>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    
     <div class = "containe-fluid">
       <div id = "header">
         <h id = "company"> blink! Eyewear</h>
@@ -35,6 +45,9 @@
             <div id = "input">
               <input type="text"  name = "email_input" class="email_input focus" id="usr" placeholder = "" value = "">
             </div>
+                <!-- Your share button code -->
+           <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="icon"></div>
+            
           </div>  
         </div>
       </div>
